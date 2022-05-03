f01e5333767a7e41a5008d46ad87d87b8c92cba1
Matthew Smilansky <msmilansky@gmail.com>
2022-05-02 20:11:22 -0700

    Modified landing page.

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Flex, Box, Image } from '@chakra-ui/react';
+import { Flex, Heading, Box, Image } from '@chakra-ui/react';
+import iphoneImg from './iPhone.png';
 
-import landing from './landing_background.jpg';
 const Landing = (props) => {
-  const { errors, label } = props;
   return (
-    <Flex>
-      <Box>
-        <Image src={landing} height="100vh" width="auto" />
-      </Box>
+    <Flex
+      gap={{ md: '14rem', sm: '5rem' }}
+      h="100vh"
+      w="100vw"
+      background="linear-gradient(28deg, rgba(2,0,36,1) 0%, rgba(82,0,177,1) 50%, rgba(0,212,255,1) 100%)"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        gap={40}
+        align="center"
+        justify="center"
+        mx={20}
+        direction={{ md: 'row', sm: 'column' }}
+      >
+        <Image w="50%" src={iphoneImg} />
+        <Heading textAlign="center" fontSize={80}>
+          Use your time wisely.
+        </Heading>
+      </Flex>
     </Flex>
   );
 };
