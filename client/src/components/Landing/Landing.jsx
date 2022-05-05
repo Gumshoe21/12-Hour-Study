42d58b255ddf3fc882a9ba9fe074db0129e1f172
Matthew Smilansky <msmilansky@gmail.com>
2022-05-04 19:37:49 -0700

    Added animations to Landing component.

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Flex, Text, Grid, Heading, Box, Image } from '@chakra-ui/react';
+import {
+  keyframes,
+  Flex,
+  Text,
+  Grid,
+  Heading,
+  Box,
+  Image,
+  Button
+} from '@chakra-ui/react';
 import iphoneImg from './iPhone.png';
 
 const Landing = (props) => {
+  const fadeIn = keyframes`
+    0% { opacity:0; }
+    66% { opacity:0; }
+    100% { opacity:1; }
+`;
   return (
     <Box background="linear-gradient(28deg, rgba(2,0,36,1) 0%, rgba(82,0,177,1) 50%, rgba(0,212,255,1) 100%)">
       <Grid
@@ -13,7 +27,14 @@ const Landing = (props) => {
         maxW="130rem"
         h="100vh"
       >
-        <Flex order={{ md: '1', sm: '2' }} justify="center" align="center">
+        <Flex
+          sx={{
+            animation: `3s ${fadeIn}`
+          }}
+          order={{ md: '1', sm: '2' }}
+          justify="center"
+          align="center"
+        >
           <Image w="50%" src={iphoneImg} />
         </Flex>
         <Box
@@ -26,14 +47,41 @@ const Landing = (props) => {
             color="whiteAlpha.900"
             textAlign="center"
             fontSize={48}
+            animationDelay="2s"
+            sx={{
+              animation: `1s ${fadeIn}`
+            }}
           >
             Spend your time wisely.
           </Heading>
-          <Text fontSize={24} textAlign="center">
+          <Text
+            sx={{
+              animation: `1.5s ${fadeIn}`
+            }}
+            fontSize={24}
+            textAlign="center"
+            mb={12}
+          >
             Whether you're doing homework, studying for a test, or learning just
             for the heck of it, <em>12 Hour Study</em> gives you the time of
             day.
           </Text>
+          <Flex
+            align="center"
+            justify="center"
+            sx={{
+              animation: `2s ${fadeIn}`
+            }}
+            gap={8}
+          >
+            <Button bg="purple.500" fontSize={18} py={10} px="3rem">
+              Sign Up
+            </Button>
+
+            <Button bg="purple.500" fontSize={18} py={10} px="3rem">
+              Log In
+            </Button>
+          </Flex>
         </Box>
       </Grid>
     </Box>
