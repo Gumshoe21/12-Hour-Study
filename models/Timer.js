96b2ba09232d741221ac85441f42cd34470fb206
Matthew Smilansky <msmilansky@gmail.com>
2022-05-13 23:05:27 -0700

    Added audioPlayer.js utility; implement working ticking audio to Timer component.

---
+++ models/Timer.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TimerSchema = new mongoose.Schema({
+  modes: {
+    session: {
+      id: {
+        type: String,
+        default: 'session'
+      },
+      name: {
+        type: String,
+        default: 'Session'
+      },
+      length: {
+        type: Number,
+        default: 30
+      }
+    },
+    shortBreak: {
+      id: {
+        type: String,
+        default: 'shortBreak'
+      },
+      name: {
+        type: String,
+        default: 'Short Break'
+      },
+      length: {
+        type: Number,
+        default: 5
+      }
+    },
+
+    longBreak: {
+      id: {
+        type: String,
+        default: 'longBreak'
+      },
+      name: {
+        type: String,
+        default: 'Long Break'
+      },
+      length: {
+        type: Number,
+        default: 15
+      }
+    }
+  },
+  longBreakInterval: {
+    id: 'longBreakInterval',
+    type: Number,
+    default: 4
+  },
+  activeMode: {
+    type: String,
+    default: 'session'
+  },
+  ticking: {
+    type: Boolean,
+    default: false
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model('Timer', TimerSchema);
---
+++ utils/email.js
@@ -0,0 +1,57 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { htmlToText } = require('html-to-text');
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.url = url;
+    this.from = `Matthew Smilansky <${process.env.EMAIL_FROM}>`;
+  }
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return 1;
+    }
+    return nodemailer.createTransport({
+      // service: 'Gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
+  async send(template, subject) {
+    // Send the actual email - Render HTML based on a template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        to: this.to,
+        url: this.url,
+        subject
+      }
+    );
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html)
+      // 3) create transport and send email
+    };
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  // email actions
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to 12 Hour Study!');
+  }
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Reset Your Password - 12 Hour Study (valid for only 10 minutes)'
+    );
+  }
+};
