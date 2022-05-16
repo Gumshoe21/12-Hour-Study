5431021a4dd3539749878ec4884a5124572e6d28
Matthew Smilansky <msmilansky@gmail.com>
2022-05-15 18:58:58 -0700

    Changed Logo Link from chakra-ui Link to react-router-dom Link.

--- client/src/components/UI/Logo/Logo.js
+++ client/src/components/UI/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Flex, Text, Link } from '@chakra-ui/react';
+import { Link as ReactLink } from 'react-router-dom';
 import { OwlIcon } from './OwlIcon';
 
 const Logo = () => {
   return (
-    <Link href="/dashboard">
+    <Link as={ReactLink} to="/dashboard">
       <Flex align="center" justify="center" columnGap={2}>
         <OwlIcon w={10} h={10} fill="purple.800" />
         <Text fontSize={14} fontWeight="700">
