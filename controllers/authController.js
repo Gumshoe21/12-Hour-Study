62d575df7acdc90bea97a6aa3da94bf57383e6ed
Matthew Smilansky <msmilansky@gmail.com>
2022-05-21 12:26:13 -0700

    Removed getUser from App.

--- controllers/authController.js
+++ controllers/authController.js
@@ -24,7 +24,7 @@ const createSendToken = (user, statusCode, req, res) => {
   const cookieOptions = {
     // expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     httpOnly: true,
-    sameSite: 'none',
+    SameSite: 'none',
     secure: true
   };
 
