9af0e1c1322f77f93ebc057e77ace2e631592551
Matthew Smilansky <msmilansky@gmail.com>
2022-05-20 17:30:06 -0700

    Added get request back to auth actions.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -100,7 +100,7 @@ exports.logout = (req, res) => {
 
 exports.protect = catchAsync(async (req, res, next) => {
   //1) Getting token and check if it's there
-  console.log(req);
+  console.log(req.headers);
   let token;
   if (
     req.headers.authorization &&
