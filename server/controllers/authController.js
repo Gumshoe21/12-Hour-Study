0e33b17f4420da57b5512ec66b3dfe4c13c0c3ab
Matthew Smilansky <msmilansky@gmail.com>
2022-05-21 12:00:51 -0700

    Removed userController code.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,7 +23,8 @@ const createSendToken = (user, statusCode, req, res) => {
 
   const cookieOptions = {
     // expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    httpOnly: true
+    httpOnly: true,
+    sameSite: 'none'
   };
 
   res.cookie('jwt', token, cookieOptions);
