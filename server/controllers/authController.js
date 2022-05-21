1775f8953d6efe46b441b7f88c2953d9c4bfb42f
Matthew Smilansky <msmilansky@gmail.com>
2022-05-21 11:43:29 -0700

    Removed userController code.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -22,7 +22,7 @@ const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user.id);
 
   const cookieOptions = {
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    // expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     httpOnly: true
   };
 
