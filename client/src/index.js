700cca70e7667d020ed92374b7e61aa54aa2b069
Matthew Smilansky <msmilansky@gmail.com>
2022-07-04 12:02:23 -0700

    Added focus-visible package to prevent blue accessability box-shadow outlines from appearing unless a user is using their keyboard to navigate.

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import 'focus-visible/dist/focus-visible';
+import { Global, css } from '@emotion/react';
 import { Provider } from 'react-redux';
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
-import { mode } from "@chakra-ui/theme-tools"
+import { mode } from '@chakra-ui/theme-tools';
 import ReactDOM from 'react-dom';
 import App from './App';
 import store from './store/index';
 import '@fontsource/inter/700.css';
 import '@fontsource/source-sans-pro/400.css';
 import '@fontsource/raleway/600.css';
-import theme from "./theme"
+import theme from './theme';
 
 // COLORS
 const buttonBgDark = 'purple.700';
 const buttonBgLight = 'gray.500';
 
 // THEME
-
+const GlobalStyles = css`
+  /*
+    This will hide the focus indicator if the element receives focus    via the mouse,
+    but it will still show up on keyboard focus.
+  */
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 ReactDOM.render(
   <Provider store={store}>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.initialColorMode} />
+      <Global styles={GlobalStyles} />
       <App />
     </ChakraProvider>
   </Provider>,
